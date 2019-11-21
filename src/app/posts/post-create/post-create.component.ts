f023af6403fd29d64cbea7ab85b474b843ebdaae
Abhay Purohit <AbhayPurohit@Etechaces.com>
2019-11-21 19:07:14 +0530

    commit

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Post } from '../post.model';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms'
+import { PostsService } from '../posts.service';
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -8,20 +9,17 @@ import { NgForm } from '@angular/forms'
 })
 export class PostCreateComponent implements OnInit {
   
-  constructor() { }
+  constructor(public postsService: PostsService) { }
   enteredTitle = "";
   enteredContent = "";
-  @Output() postCreated = new EventEmitter<Post>();
+  
   
   onAddPost(form: NgForm){
     if(form.invalid){
       return;
     }
-    const post: Post ={
-      title: form.value.title, 
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
+   
+    this.postsService.addPost(form.value.title, form.value.content);
    
   }
   ngOnInit() {
