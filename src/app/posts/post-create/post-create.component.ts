dd0be70199d26c389b1d42aa27ae40517c692ff3
Abhay Purohit <AbhayPurohit@Etechaces.com>
2019-11-22 18:13:01 +0530

    commit

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -20,6 +20,7 @@ export class PostCreateComponent implements OnInit {
     }
    
     this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
    
   }
   ngOnInit() {
